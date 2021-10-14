653ed76f76f2d11705816b16dad0187510c8da74
sudhakartuta <sudhakar.tuta@gmail.com>
2021-10-14 18:45:01 +0530

    Created Catalog API

---
+++ src/Service/Catalog/Catalog.API/Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string Id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string category);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string Id);
+    }
+}
